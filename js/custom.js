cf8b6fd83a0adf5ab9f85a989e33b1575a965cd3
yueruchen <331607317@qq.com>
2018-08-01 00:18:03 -0500

    rebuilding site 2018年 8月 1日 星期三 00时18分03秒 CDT

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,3 @@
 var dom = document.getElementById("publications")
 dom.childNodes[1].childNodes[1].childNodes[3].className="col-xs-12 col-md-12"
+// dom.childNodes[1].childNodes[1].childNodes[2].className="col-xs-12 col-md-12"
