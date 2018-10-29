baad0fbc8bce768c0364dfd258675922dd140442
yueruchen <331607317@qq.com>
2018-10-29 19:33:42 +0800

    rebuilding site 2018年10月29日 星期一 19时33分42秒 CST

--- js/custom.js
+++ js/custom.js
@@ -3,13 +3,13 @@ dom.childNodes[1].childNodes[1].childNodes[3].className="col-xs-12 col-md-12";
 // dom.childNodes[1].childNodes[1].childNodes[2].className="col-xs-12 col-md-12"
 dom1 = dom.childNodes[1].childNodes[1].childNodes[3].childNodes[1].childNodes[7];
 var p = document.createElement('p');
-p.innerHTML = '<strong>Yifan Jiang*</strong>, Xiaoye Qu*, Xi Ouyang, Yu Cheng, Shiping Wen, Yang Yang, Pan Zhou';
+p.innerHTML = '<strong>Yifan Jiang*</strong>, Xiaoye Qu*, <a href="https://oyxhust.github.io/">Xi Ouyang</a>, <a href="https://sites.google.com/site/chengyu05/">Yu Cheng</a>, Shiping Wen, Yang Yang, Pan Zhou';
 dom1.innerText = "";
 dom1.appendChild(p);
 
 dom2 = dom.childNodes[1].childNodes[1].childNodes[3].childNodes[3].childNodes[7];
 var p = document.createElement('p');
-p.innerHTML = 'Xi Ouyang*, Yu Cheng*, <strong>Yifan Jiang</strong>, Chun-Liang Li, Pan Zhou';
+p.innerHTML = '<a href="https://oyxhust.github.io/">Xi Ouyang*</a>, <a href="https://sites.google.com/site/chengyu05/">Yu Cheng*</a>, <strong>Yifan Jiang</strong>, <a href="http://www.cs.cmu.edu/~chunlial/">Chun-Liang Li</a>, Pan Zhou';
 dom2.innerText = "";
 dom2.appendChild(p);
 // dom1.child = <p><strong>Yifan Jiang*</strong>, Xiaoye Qu*, Xi Ouyang, Yu Cheng, Shiping Wen, Yang Yang, Pan Zhou </p>;
