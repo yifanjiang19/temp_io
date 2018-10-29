a1482512a7868e66de270e87ea1092312cba72cd
yueruchen <331607317@qq.com>
2018-10-29 19:11:33 +0800

    rebuilding site 2018年10月29日 星期一 19时11分33秒 CST

--- js/custom.js
+++ js/custom.js
@@ -2,4 +2,14 @@ var dom = document.getElementById("publications");
 dom.childNodes[1].childNodes[1].childNodes[3].className="col-xs-12 col-md-12";
 // dom.childNodes[1].childNodes[1].childNodes[2].className="col-xs-12 col-md-12"
 dom1 = dom.childNodes[1].childNodes[1].childNodes[3].childNodes[1].childNodes[7];
-dom1.innerText = "<b>Yifan Jiang*</b>, Xiaoye Qu*, Xi Ouyang, Yu Cheng, Shiping Wen, Yang Yang, Pan Zhou";
+var p = document.createElement('p');
+p.innerHTML = '<strong>Yifan Jiang*</strong>, Xiaoye Qu*, Xi Ouyang, Yu Cheng, Shiping Wen, Yang Yang, Pan Zhou';
+dom1.innerText = "";
+dom1.appendChild(p);
+
+dom2 = dom.childNodes[1].childNodes[1].childNodes[3].childNodes[3].childNodes[7];
+var p = document.createElement('p');
+p.innerHTML = 'Xi Ouyang*, Yu Cheng*, <strong>Yifan Jiang</strong>, Chun-Liang Li, Pan Zhou';
+dom2.innerText = "";
+dom2.appendChild(p);
+// dom1.child = <p><strong>Yifan Jiang*</strong>, Xiaoye Qu*, Xi Ouyang, Yu Cheng, Shiping Wen, Yang Yang, Pan Zhou </p>;
