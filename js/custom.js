d2ab53df2b3ced6c18ff8ec84031dfd093f9f3ce
yueruchen <331607317@qq.com>
2018-11-19 10:47:28 +0800

    rebuilding site 2018年11月19日 星期一 10时47分28秒 CST

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,14 @@
 var dom = document.getElementById("publications");
 dom.childNodes[1].childNodes[1].childNodes[3].className="col-xs-12 col-md-12";
 // dom.childNodes[1].childNodes[1].childNodes[2].className="col-xs-12 col-md-12"
-dom1 = dom.childNodes[1].childNodes[1].childNodes[3].childNodes[1].childNodes[7];
+
+dom1 = dom.childNodes[1].childNodes[1].childNodes[3].childNodes[3].childNodes[7];
 var p = document.createElement('p');
 p.innerHTML = '<strong>Yifan Jiang*</strong>, Xiaoye Qu*, <a href="https://oyxhust.github.io/" target="view_window">Xi Ouyang</a>, <a href="https://sites.google.com/site/chengyu05/" target="view_window">Yu Cheng</a>, Shiping Wen, Yang Yang, Pan Zhou';
 dom1.innerText = "";
 dom1.appendChild(p);
 
-dom2 = dom.childNodes[1].childNodes[1].childNodes[3].childNodes[3].childNodes[7];
+dom2 = dom.childNodes[1].childNodes[1].childNodes[3].childNodes[5].childNodes[7];
 var p = document.createElement('p');
 p.innerHTML = '<a href="https://oyxhust.github.io/" target="view_window">Xi Ouyang*</a>, <a href="https://sites.google.com/site/chengyu05/" target="view_window">Yu Cheng*</a>, <strong>Yifan Jiang</strong>, <a href="http://www.cs.cmu.edu/~chunlial/" target="view_window">Chun-Liang Li</a>, Pan Zhou';
 dom2.innerText = "";
